c004dca7384727d0b6aca309abac1f6077ded475
Yarmoshevi4 <yarmoshevi4@gmail.com>
2017-05-20 15:51:41 +0300

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,52 @@
+#print('Hello, Django Girl!')
+#if 3 > 2:
+#	print ('it works!')
+#if 5 > 2: 
+#	print ('5 is indeed greater than 2')
+#else:
+#	print ('5 is not greater than 2')
+#name = 'katya'
+#if name =='ola':
+#	print ('hello, ola')
+#elif name == 'katya':
+#	print ('hello, katya')	
+#else:
+#	print ('hello, world')
+#volume = 20
+#if volume < 3:
+#	print ('greate')
+#elif volume < 32 and volume != 21:
+#	print ('olalala')
+#elif volume > 24:
+#	print ('cool')
+#elif 21 <= volume <= 25:
+#	print ('you know this task')
+#def hi():
+#	print ('hey, there!')
+#	print ('how are u?')
+#hi ()
+#def hi (name): 
+#	if name == 'ola':
+#		print ('hey, ola')
+#	elif name == 'katya':
+#		print ('hey, katya')
+#	else:
+#		print ('hi anywhere')
+
+#Hi('')
+
+#def hi (name):
+#	print ('hey ' + name + '!')
+
+#hi ("Ira")
+
+#def hi (name):
+#	print ('hey ' + name +'!')
+
+#girls = ['katya', 'nadya', 'ira', 'nastya']
+#for name in girls:
+#	hi(name)
+#	print ('Next girl')
+	
+for i in range(1,10):
+	print (i)
